63c766deebd5fb973c949c40fc8e5df0149ba645
knowocien0 <knowocien12@gmail.com>
2025-01-07 08:13:34 +0100

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 
 
-let znienna_a = 1;
-znienna_a = znienna_a + 15;
-
-
-console.log(znienna_a, typeof(znienna_a));
-
-let pobrana_zmienna = prompt("Podaj znaczenia")
-console.log(pobrana_zmienna, typeof(pobrana_zmienna));
+let znienna_a =parseFloat(prompt("podaj a"));
+znienna_a = znienna_a - 1;
+alert(znienna_a);
