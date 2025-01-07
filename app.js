c8d277ab61d1fcbe9a15b76795178c813b0a2458
knowocien0 <knowocien12@gmail.com>
2025-01-07 07:48:28 +0100

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("dziala")
+
+
+let znienna_a = 1;
+znienna_a = znienna_a + 15;
+
+
+console.log(znienna_a, typeof(znienna_a));
+
+let pobrana_zmienna = prompt("Podaj znaczenia")
+console.log(pobrana_zmienna, typeof(pobrana_zmienna));
